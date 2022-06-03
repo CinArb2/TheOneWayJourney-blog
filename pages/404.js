91dcee1b3469732d8db8b7a07d586875ea85af96
CinArb2 <91969387+CinArb2@users.noreply.github.com>
2022-06-03 16:59:04 -0500

    fix 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import styles from '../styles/NotFound.module.css'
 import Layout from '../comps/Layout'
-import { getAllMenus, getLogo, getCategoryFooter } from '../lib/api'
+import { getAllMenus, getLogo, getCategoryFooter, getCategories } from '../lib/api'
 
 
-export default function NotFound({  menu, logo, categoryFooter }) {
+export default function NotFound({ logo, categories}) {
   
   return (
-    <Layout menu={menu} logo={logo} categoryFooter={categoryFooter}>
+    <Layout menu={categories} logo={logo}>
       <div className={styles.container}>
         <h1>404</h1>
         <p>It looks like nothing was found at this location. <Link href="/"><a>Return to Home Page</a></Link></p>
@@ -18,15 +18,13 @@ export default function NotFound({  menu, logo, categoryFooter }) {
 
 export async function getStaticProps() {
   
-  const menus = await getAllMenus()
   const logo = await getLogo()
-  const footer = await getCategoryFooter()
+  const categories = await getCategories()
 
   return {
     props: {
-      menu: menus?.nodes[0].menuItems.edges,
-      logo: logo?.nodes[0].sourceUrl,
-      categoryFooter: footer?.nodes[0].menuItems.nodes
+      categories,
+      logo: logo?.[0].logoImage.url,
     },
     revalidate: 10,
   }
