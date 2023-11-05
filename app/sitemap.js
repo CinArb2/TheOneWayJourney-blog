8070b08686b91dcd78376c63e0acc70cc289c7ea
CinArb2 <cindypao27@gmail.com>
2023-11-05 10:56:34 -0500

    feat: move sitemap page to app router

---
+++ app/sitemap.js
@@ -0,0 +1,17 @@
+import { allSlugs } from '../shared/queries'
+import { fetchData } from '../shared/server/gql.server'
+
+const URL = process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000'
+
+export default async function sitemap() {
+  const { posts } = await fetchData(allSlugs)
+  const baseRoutes = [
+    { url: URL },
+    { url: `${URL}/about` },
+    { url: `${URL}/contact` },
+  ]
+
+  const slugRoutes = posts.map((slug) => ({ url: `${URL}/${slug?.slug}` }))
+
+  return [...baseRoutes, ...slugRoutes]
+}
