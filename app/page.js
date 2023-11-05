375819e38690a16a7424a7f9f0c5c35af428d1e8
CinArb2 <cindypao27@gmail.com>
2023-11-05 08:57:24 -0500

    feat: transform author in server component

--- app/page.js
+++ app/page.js
@@ -1,28 +1,25 @@
 import React from 'react'
-// import Layout from '../comps/Layout'
 import Hero from '../comps/Hero'
 import styles from '../styles/Home.module.css'
 import PostCard from '../comps/PostCard'
 import FeaturedPosts from '../comps/FeaturedPosts'
-import Author from '../comps/Author'
+import Author from './components/author'
 import Tags from '../comps/Tags'
 import { fetchData } from '../shared/server/gql.server'
 import { posts, featuredPosts, author, tags } from '../shared/queries'
 
 async function getPosts() {
   try {
-    const [listPosts, responseFeaturedPosts, responseAuthor, responseTags] =
-      await Promise.all([
-        fetchData(posts),
-        fetchData(featuredPosts),
-        fetchData(author),
-        fetchData(tags),
-      ])
+    const [listPosts, responseFeaturedPosts, responseTags] = await Promise.all([
+      fetchData(posts),
+      fetchData(featuredPosts),
+      fetchData(author),
+      fetchData(tags),
+    ])
 
     return {
       ...listPosts,
       featuredPosts: responseFeaturedPosts?.posts,
-      ...responseAuthor,
       ...responseTags,
     }
   } catch (error) {
@@ -33,7 +30,7 @@ async function getPosts() {
 }
 
 export default async function Home() {
-  const { posts, featuredPosts, authors: author, tags } = await getPosts()
+  const { posts, featuredPosts, tags } = await getPosts()
 
   return (
     <>
@@ -47,7 +44,7 @@ export default async function Home() {
         <aside className={styles.containerAside}>
           <FeaturedPosts featuredPosts={featuredPosts} />
           <Tags tags={tags} />
-          <Author author={author} />
+          <Author />
         </aside>
       </div>
     </>
