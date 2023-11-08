e2ee1501cdaf7ddf22444ad7da8b2c76b51c7184
CinArb2 <cindypao27@gmail.com>
2023-11-07 20:22:25 -0500

    feat: move link component to the top of the featured post component

--- components/feature-individual.tsx
+++ components/feature-individual.tsx
@@ -5,28 +5,28 @@ import Link from 'next/link'
 
 const FeaturedIndiv = ({ post }: { post: Post }) => {
   return (
-    <div className={styles.wrapperFeatured}>
-      <div className={styles.imageWrapper}>
-        <Image
-          src={post.featuredImage.url}
-          alt="icon-logo"
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: 'cover',
-          }}
-        />
-      </div>
-      <div className={styles.FeaturedBody}>
-        <Link href={`/post/${post.slug}`}>
+    <Link href={`/post/${post.slug}`}>
+      <div className={styles.wrapperFeatured}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={post.featuredImage.url}
+            alt="icon-logo"
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <div className={styles.FeaturedBody}>
           <h3>{post.title}</h3>
-        </Link>
-        <p className={styles.FeaturedAuthor}>
-          <img src="https://img.icons8.com/ios/50/000000/user--v1.png" />
-          {post.author?.name}
-        </p>
+          <p className={styles.FeaturedAuthor}>
+            <img src="https://img.icons8.com/ios/50/000000/user--v1.png" />
+            {post.author?.name}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
