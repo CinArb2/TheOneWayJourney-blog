362784a2106a3c68abae6a8c165ee3043ec04815
CinArb2 <cindypao27@gmail.com>
2023-11-11 13:29:06 -0500

    feat: remove dynamic param false

--- app/(overview)/category/[categoryName]/page.tsx
+++ app/(overview)/category/[categoryName]/page.tsx
@@ -5,7 +5,6 @@ import { fetchData } from '@/shared/server/gql.server'
 import { categories, postsByCategory } from '@/shared/queries'
 import { getPosts } from '@/shared/server/get-posts'
 
-export const dynamicParams = false
 interface Categorie {
   slug: string
   name: string
